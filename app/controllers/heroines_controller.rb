45029eaa5e38fb861bdbec1b381d49bb3318f403
github username <github email address>
2019-03-01 11:00:46 -0800

    Done.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,53 @@
 class HeroinesController < ApplicationController
+  before_action :is_search?
   def index
     @heroines = Heroine.all
+    # byebug
+    @powers = Power.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+
+  end
+
+  def search
+    unless search_params.blank?
+      @heroines = Heroine.where(power_id: search_params)
+      # byebug
+      @powers = Power.all
+      render :index
+    else
+      @heroines = Heroine.all
+      render :index
+    end
+  end
+
+  def is_search?
+    if params[:commit] == "Search"
+      search
+    end
+  end
+
+  private
+    def heroine_params
+      params.require(:heroine).permit(:name, :super_name, :power_id)
+    end
+    def search_params
+      params.require(:search)
+    end
 end
