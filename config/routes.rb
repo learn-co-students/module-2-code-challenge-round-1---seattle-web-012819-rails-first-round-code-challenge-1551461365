9e7b271874f5d02da7d4884d153479e3eec5b508
Madison Stankevich <madison.stankevich@flatironschool.com>
2019-03-01 11:00:12 -0800

    Nailed it.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :powers #, only: [:index]
+  resources :heroines #, only: [:index]
+
+  get '/search', to: 'heroines#search' #I am not sure this is the correct route.
+  #Attempting to redirect to the index but with filtered results...not sure how
 end
