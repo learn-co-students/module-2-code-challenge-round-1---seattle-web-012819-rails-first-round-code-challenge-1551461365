1eb3334ced017b93e73dfe58cd8ced3f426c6669
ScottDenton <scottyd_14@hotmail.com>
2019-03-01 10:04:59 -0800

    All initial specs complete

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,29 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+
+    redirect_to @heroine
+  end
+
+  def show
+    set_heroine
+  end
+
+
+  private
+
+  def set_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,14 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    set_power
+  end
+
+  private
+
+  def set_power
+    @power = Power.find(params[:id])
+  end
 end
