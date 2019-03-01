2604032b6daf3aebf92a43bb6240740f429d360b
omfgtora <spaminol@gmail.com>
2019-03-01 11:00:06 -0800

    Nailed it.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,31 @@
-class HeroinesController < ApplicationController
-  def index
-    @heroines = Heroine.all
-  end
+class HeroinesController < ApplicationController     
+     def index 
+        @heroines = Heroine.all
+     end 
+
+     def show
+      @heroine = Heroine.find(params[:id])
+     end
+  
+     def new 
+        @heroine = Heroine.new 
+        @powers = Power.all
+     end 
+  
+     def create
+      @heroine = Heroine.create(heroine_params)
+      if @heroine.valid?
+        flash[:success] = "New Heroine successfull created!"
+        redirect_to heroine_path(@heroine)
+      else
+        flash[:errors] = "Something went wrong"
+        redirect_to new_heroine_path
+      end
+     end 
+  
+     private 
+  
+     def heroine_params 
+        params.require(:heroine).permit(:name, :super_name, :id, :power_id)
+     end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,9 @@
 class Heroine < ApplicationRecord
+  has_one :power
+
+  validates :name, uniqueness: true
+
+  def power
+    power = Power.find(self.power_id)
+  end
 end
