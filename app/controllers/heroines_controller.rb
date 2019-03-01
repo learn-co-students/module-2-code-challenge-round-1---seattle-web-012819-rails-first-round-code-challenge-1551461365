010dade651943710b7c08cbc09da41ec7a17ccfb
github username <allen.woods@flatironschool.com>
2019-03-01 10:50:05 -0800

    Crossing my fingers...

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,47 @@
 class HeroinesController < ApplicationController
-  def index
+  before_action :all_heroines, only: [:index]
+  before_action :new_heroine, only: [:new]
+  before_action :find_heroine, only: [:show, :edit, :update]
+  
+  # The following methods are handled by before_action:
+
+  # implied index
+  # implied new
+  # implied show
+  # implied edit
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    unless @heroine
+      flash[:errors] << "Could not create that heroine"
+    else
+      redirect_to @heroine
+    end
+  end
+
+  def update
+    unless @heroine.update(heroine_params)
+      flash[:errors] << "Could not update that heroine"
+    else
+      redirect_to @heroine
+    end
+  end
+
+  private
+
+  def all_heroines
     @heroines = Heroine.all
+  end
+
+  def new_heroine
+    @heroine = Heroine.new
+  end
+
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
