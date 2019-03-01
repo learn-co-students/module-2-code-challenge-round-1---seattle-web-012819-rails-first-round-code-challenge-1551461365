3699ad4aa0fbfd17f4518e0b2516fc5f6c38d62a
github username <github email address>
2019-03-01 11:02:19 -0800

    rad this

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,6 @@
 class Heroine < ApplicationRecord
+   belongs_to :power
+   validates :super_name, uniqueness: true
+   #ideally would create a custom validation that downcases and checks for a downcase
+   #match.
 end
