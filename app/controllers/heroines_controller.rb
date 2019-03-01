fbea1b7efc4879cf52cb2989bb5f3bec4d564cb3
halented <hallie.dunn1@gmail.com>
2019-03-01 11:01:46 -0800

    SUPA POWAS

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,40 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    @heroines = Heroine.search(params[:search])
+  end
+
+  # def search
+  #   binding.pry
+  #   power = Power.find(params[:power])
+  #   @heroines = []
+  #   Heroine.all.each do |her|
+  #     if her.power == power
+  #       @heroines << her
+  #     end
+  #   end
+  # end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to @heroine
+    else 
+      flash[:notice] = "Your name must be unique!"
+      redirect_to new_heroine_path
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id, :search)
   end
 end
