b6e426219a16a0adae425b2bbd3413d3f52dc537
Emily Wagner <emilywagner598@gmail.com>
2019-03-01 11:00:02 -0800

    Added a few more features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :powers, only: [:index, :show]
   resources :heroines, only: [:index, :show, :new, :create]
-
+  
    get '/search' => 'heroines#search'
 end
