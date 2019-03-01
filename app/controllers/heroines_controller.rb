c99543cac26f592efe9db07b922c98627a7e0383
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-03-01 11:00:14 -0800

    NAILED IT

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,34 @@
 class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
+    power = Power.find_by(name: params["q"])
+    if power
+      @power_heroines = Heroine.where(power_id: power.id)
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    heroine = Heroine.create(heroine_params)
+    if heroine.valid?
+      redirect_to heroine_path(heroine)
+      flash[:notice] = "Heroine Successfully Created"
+    else
+      redirect_to new_heroine_path
+      flash[:notice] = "A Heroine with that Super Name already exists, please try again."
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,30 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+    @heroines = Heroine.where(power_id: @power.id)
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    power = Power.create(power_params)
+    if power.valid?
+      redirect_to power_path(power)
+      flash[:notice] = "Power Successfully Created"
+    else
+      redirect_to new_power_path
+      flash[:notice] = "That Power already exists, please try again."
+    end
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
 end
